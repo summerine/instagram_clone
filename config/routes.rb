6cf5443c7cdb4863e16de3935d77b5eedef47f2a
indri-pr <indri.pr.26@gmail.com>
2019-09-09 04:54:42 +0700

    editing app layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'home/index'
 
   root to: 'home#index'
+  resource :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
